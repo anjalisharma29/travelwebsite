1acbc9c3feffa6da0a0364e1349f404c098f0c9e
Anjali Sharma <shaarmaanjali2429@gmail.com>
2021-12-30 20:59:37 +0530

    Add files via upload

---
+++ src/components/pages/Error.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Error() {
+  return <h1 className='error'>ERROR 404 Page Not Found</h1>;
+}
---
+++ src/components/pages/Products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+
+export default function Products() {
+  return <>
+  
+    <h1>Products</h1>
+    <Cards/>
+  </>;
+}
